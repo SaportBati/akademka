e6686f39652fcdef62965e67857c7da97f76dd97
SaportBati <grebenkinmatveyvyceslacovi2007@gmail.com>
2026-08-17 06:21:59 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Адрес вашего Cloudflare Worker (Backend API + D1)
+// Меняйте только эту строку, если адрес воркера изменится.
+const API_BASE = "https://frosty-shadow-e915.aaf60251.workers.dev";
